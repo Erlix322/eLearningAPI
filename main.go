a17aedcc01392ec18a73e75aac8185fb2d950dac
Eric Brandt <ericbrandt@gmx.de>
2018-03-01 13:23:26 +0100

    refactoring

--- main.go
+++ main.go
@@ -40,44 +40,6 @@ func ErrorHandler(res http.ResponseWriter, req *http.Request){
 }
 
 
-func checkToken(token string) (bool, string){
-	tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-	client := &http.Client{
-		Timeout: time.Second * 10,
-		Transport:tr,
-	}
-	req, err := http.NewRequest("GET","https://api.brandt-projects.de/auth/realms/eLearning/protocol/openid-connect/userinfo",nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	req.Header.Add("Authorization", "Bearer " + token)
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-    bodyString := string(body)
-	fmt.Println("String",bodyString)
-	defer resp.Body.Close()
-	
-	c := make(map[string]interface{})
-	e := json.Unmarshal([]byte(bodyString),&c)
-	if e != nil {
-		fmt.Println("Error parsing string")
-	}
-
-	if k, ok := c["sub"]; ok{
-		return true,k.(string)
-	}
-
-	return false, "invalid token"
-	
-
-}
-
-
 
 func Middleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request){
