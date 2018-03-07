e2a2c8db62b35a0628cfa9986f89df9ef9100598
Eric Brandt <ericbrandt@gmx.de>
2018-03-07 19:51:57 +0100

    gorm mappings added

--- psql/Video.go
+++ psql/Video.go
@@ -2,9 +2,9 @@ package psql
 
 type Video struct{
 	Id int
-	Name string
-	Owner string
-	Beschreibung string
+	Name string `gorm:"column:Name"`
+	Owner string `gorm:"column:Owner"`
+	Beschreibung string `gorm:"column:Owner"`
 }
 
 type Videos struct{
