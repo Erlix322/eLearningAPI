956031dd218c04710c45a94d01cd6b9ac0785298
alf <ericbrandt@gmx.de>
2018-03-12 13:57:51 +0100

    Playlist

--- psql/PlaylistQuery.go
+++ psql/PlaylistQuery.go
@@ -9,7 +9,7 @@ import (
 )
 
 
-func (c *Connection) SavePlaylist(pl pogo.VideoPlaylists){
+func (c *Connection) SavePlaylist(pl pogo.VideoPlaylists) bool{
 	db, err := gorm.Open("mysql",c.connsTr)
 	if err != nil {
 		log.Fatal(err)
@@ -17,18 +17,23 @@ func (c *Connection) SavePlaylist(pl pogo.VideoPlaylists){
 	tx := db.Begin()
 	/*create playlist*/
 	fmt.Printf("WTF: %+v\n", pl.VideoPlaylist[0].PKVideo)
-	err = db.Create(&pl.Playlist).Error
-    /*create VideoPlaylist*/
+	if err := db.Create(&pl.Playlist).Error; err != nil {
+		tx.Rollback()
+		return false
+	}
+	/*create VideoPlaylist*/
+	
 	for index,playlist := range pl.VideoPlaylist{
 		fmt.Println(playlist)
 		play := pogo.VideoPlaylist{PKVideo:pl.VideoPlaylist[index].PKVideo,PKPlaylist:pl.VideoPlaylist[index].PKPlaylist}
-		err = db.Table("VideoPlaylist").Create(&play).Error
+		if err := db.Table("VideoPlaylist").Create(&play).Error; err != nil{
+			tx.Rollback()
+			return false
+		}
 	}
-	if err != nil{
-		tx.Rollback()
-	}else{
-		tx.Commit()
-	}
+	
+	tx.Commit()
+	return true
 	
 
 }
